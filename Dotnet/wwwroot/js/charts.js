28e96c45878704fc5950e9b6d6258ed64cb494d2
AG-kp <kwinten.pardon@aginsurance.be>
2024-02-17 23:51:49 +0100

    backend data in frontend

--- Dotnet/wwwroot/js/charts.js
+++ Dotnet/wwwroot/js/charts.js
@@ -3,10 +3,7 @@ ctx = document.getElementById('myChart');
 var labels = []
 var data = []
 
-function set_data(new_labels, new_data){
-  labels = new_labels;
-  data = new_data;
-}
+
 
   chart = new Chart(ctx, {
     type: 'line',
@@ -22,19 +19,27 @@ function set_data(new_labels, new_data){
       scales: {
         y: {
         min: 55,
-        max: 75,
+        max: 120,
         }
       }
     }
   });
 
-  interval = setInterval(function() {
-    time = new Date();
-    if(chart.data.labels.length > 20) {
-        chart.data.labels.shift();
-        chart.data.datasets[0].data.shift();
-    }
-    chart.data.labels.push(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-    chart.data.datasets[0].data.push(Math.random()*10 + 60);
-    chart.update();
-}, 5000);
+
+  function set_data(new_labels, new_data){
+    labels.push(...new_labels);
+    data.push(...new_data);
+    console.log(labels, data)
+    chart.update()
+  }
+
+//   interval = setInterval(function() {
+//     time = new Date();
+//     if(chart.data.labels.length > 20) {
+//         chart.data.labels.shift();
+//         chart.data.datasets[0].data.shift();
+//     }
+//     chart.data.labels.push(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+//     chart.data.datasets[0].data.push(Math.random()*10 + 60);
+//     chart.update();
+// }, 5000);
